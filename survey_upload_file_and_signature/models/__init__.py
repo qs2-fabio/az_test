8f374994ab904682af78b783f8c2f5da6c61f6d5
qs2-fabio <155531363+qs2-fabio@users.noreply.github.com>
2025-06-11 17:14:48 +0200

    Delete survey_upload_file_and_signature directory

--- survey_upload_file_and_signature/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-# Powered by Kanak Infosystems LLP.
-# © 2020 Kanak Infosystems LLP. (<https://www.kanakinfosystems.com>).
-from . import survey_question
-from . import survey_user_input_line
-from . import survey_user_input
